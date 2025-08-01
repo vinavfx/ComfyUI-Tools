9077846e9153a7b31506bb389cb625253cadb4e9
Francisco Contreras <fcocc77@gmail.com>
2025-08-01 13:30:48 -0400

    wan: update

--- wan.py
+++ wan.py
@@ -34,36 +34,52 @@ class wan22_inpainting:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {"vae": ("VAE", ),
+                             "video_image": ("IMAGE", ),
                              "start_image": ("IMAGE", ),
+                             "end_image": ("IMAGE", ),
                              "width": ("INT", {"default": 1280, "min": 32, "max": nodes.MAX_RESOLUTION, "step": 32}),
                              "height": ("INT", {"default": 704, "min": 32, "max": nodes.MAX_RESOLUTION, "step": 32}),
                              "length": ("INT", {"default": 49, "min": 1, "max": nodes.MAX_RESOLUTION, "step": 4}),
                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
                              }, }
 
+
     RETURN_TYPES = ("LATENT",)
     FUNCTION = "encode"
 
     CATEGORY = 'vina'
 
-    def encode(self, vae, width, height, length, batch_size, start_image):
-        latent = torch.zeros([1, 48, ((length - 1) // 4) + 1, height // 16,
-                             width // 16], device=comfy.model_management.intermediate_device())
-        mask = torch.ones([latent.shape[0], 1, ((length - 1) // 4) + 1, latent.shape[-2],
-                          latent.shape[-1]], device=comfy.model_management.intermediate_device())
+    def encode(self, vae, video_image, start_image, end_image, width, height, length, batch_size):
 
-        start_image = comfy.utils.common_upscale(
-            start_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
-        latent_temp = vae.encode(start_image)
-        latent[:, :, :latent_temp.shape[-3]] = latent_temp
-        mask[:, :, :latent_temp.shape[-3]] *= 0.0
+        # se dividen por 4 para que sea mas liviano
+        frames = ((length - 1) // 4) + 1
+
+        latent = torch.zeros([1, 48, frames, height // 16, width // 16], device=comfy.model_management.intermediate_device())
+        mask = torch.ones([latent.shape[0], 1, frames, latent.shape[-2], latent.shape[-1]], device=comfy.model_management.intermediate_device())
+
+        video = comfy.utils.common_upscale(video_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
+        latent_temp_video = vae.encode(video)
+        video_frames = latent_temp_video.shape[-3]
+        latent[:, :, :frames] = latent_temp_video
+        mask[:, :, :frames] = 1
+
+        #  start_image = comfy.utils.common_upscale(start_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
+        #  latent_temp_start = vae.encode(start_image)
+        #  n_start = latent_temp_start.shape[-3]
+        #  latent[:, :, :n_start] = latent_temp_start
+        #  mask[:, :, :n_start] = 0.0
+
+        #  end_image = comfy.utils.common_upscale(end_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
+        #  latent_temp_end = vae.encode(end_image)
+        #  n_end = latent_temp_end.shape[-3]
+        #  latent[:, :, -n_end:] = latent_temp_end
+        #  mask[:, :, -n_end:] = 0.0
+
 
         out_latent = {}
         latent_format = comfy.latent_formats.Wan22()
-        latent = latent_format.process_out(
-            latent) * mask + latent * (1.0 - mask)
-        out_latent["samples"] = latent.repeat(
-            (batch_size, ) + (1,) * (latent.ndim - 1))
-        out_latent["noise_mask"] = mask.repeat(
-            (batch_size, ) + (1,) * (mask.ndim - 1))
+        latent = latent_format.process_out(latent) * mask + latent * (1.0 - mask)
+        out_latent["samples"] = latent.repeat((batch_size, ) + (1,) * (latent.ndim - 1))
+        out_latent["noise_mask"] = mask.repeat((batch_size, ) + (1,) * (mask.ndim - 1))
         return (out_latent,)
+
